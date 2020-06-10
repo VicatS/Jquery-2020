57024d61d2e5dde2ac98539b268203bd91dab707
victor <victorcs2704@gmail.com>
2020-06-10 18:49:28 -0400

    97. Hover

--- js/02-events.js
+++ js/02-events.js
@@ -2,10 +2,19 @@ $(function () {
 //    mouseover and mouseout
     let box = $("#box")
 
-    box.mouseover(function () {
+    /*box.mouseover(function () {
         $(this).css('background', 'red')
     })
     box.mouseout(function () {
         $(this).css('background', 'green')
-    })
+    })*/
+
+    function changeRed() {
+        $(this).css('background', 'red')
+    }
+    function changeGreen() {
+        $(this).css('background', 'green')
+    }
+//    Hover
+    box.hover(changeRed, changeGreen)
 })
