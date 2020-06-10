8e655b29e1946eb587622d647f3d3c577edcb37d
victor <victorcs2704@gmail.com>
2020-06-10 15:53:58 -0400

    91. Selector de ID

---
+++ js/01-selectores.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+//    Selector de ID
+    let red = $("#red").css("background", "red")
+        .css("color", "white")
+    $("#yellow").css("background", "yellow")
+        .css("color", "green")
+    $("#green").css("background", "green")
+        .css("color", "white")
+
+})
