cc07076ef1b31022f43a697161f6981fd849d986
victor <victorcs2704@gmail.com>
2020-06-10 19:14:46 -0400

    99. Blur y focus

--- js/02-events.js
+++ js/02-events.js
@@ -27,4 +27,15 @@ $(function () {
         $(this).css('background', 'purple')
             .css('color', 'white')
     })
+
+//    Focus and blur
+    let name = $("#name")
+
+    name.focus(function () {
+        $(this).css("border", "2px solid green")
+    })
+    name.blur(function () {
+        $(this).css("border", "1px solid #ccc")
+        $("#info").text($(this).val()).show()
+    })
 })
