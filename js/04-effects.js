cb6edd85dc22ebdabc6e6a16e62017dc1ee3c6ad
victor <victorcs2704@gmail.com>
2020-06-11 18:32:03 -0400

    103. Animaciones personalizadas

--- js/04-effects.js
+++ js/04-effects.js
@@ -1,19 +1,45 @@
 $(function () {
+    let box = $('#box')
     $('#show').hide()
 
     $('#show').click(function () {
         $(this).hide()
         $('#hidden').show()
-        $('#box').slideDown('slow',1)
+        box.slideDown('slow', 1)
     })
 
     $('#hidden').click(function () {
         $(this).hide()
         $('#show').show()
-        $('#box').slideUp('slow',0)
+        box.slideUp('slow', 0)
     })
 
     $('#allInOne').click(function () {
-        $('#box').slideToggle('fast')
+        box.slideToggle('fast')
+    })
+
+    $('#putMeUp').click(function () {
+        box.animate({
+            marginLeft: '500px',
+            fontSize: '40px',
+            height: '100px'
+        }, 'slow')
+            .animate({
+                borderRadius: '900px',
+                marginTop: '80px'
+            }, "slow")
+            .animate({
+                borderRadius: '0px',
+                marginLeft: '0px'
+            }, "slow")
+            .animate({
+                borderRadius: '100px',
+                marginTop: '0px'
+            }, "slow")
+            .animate({
+                marginLeft: '500px',
+                fontSize: '40px',
+                height: '100px'
+            }, 'slow')
     })
 })
