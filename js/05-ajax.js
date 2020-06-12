7783e0258b4e1dc6569615009c17c0b170985d66
victor <victorcs2704@gmail.com>
2020-06-12 16:05:56 -0400

    107. Metodo Post - AJAX

--- js/05-ajax.js
+++ js/05-ajax.js
@@ -4,9 +4,24 @@ $(function () {
 
 // GET and POST
     $.get("https://reqres.in/api/users", {page: 2}, function (response) {
-        console.log(response)
         response.data.forEach((element, index) => {
             $("#data").append("<p>" + element.first_name + " " + element.last_name + "</p>")
         })
+    })
+
+    $("#form").submit(function (e) {
+        e.preventDefault()
+        let user = {
+            "name": $('input[name="name"]').val(),
+            "web": $('input[name="web"]').val()
+        }
+        console.log(user)
+        $.post($(this).attr("action"), user, function (response) {
+            console.log(response)
+        }).done(function () {
+            alert("User successfully added")
+        })
+
+        return false;
     })
 })
