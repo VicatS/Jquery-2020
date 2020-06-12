2c79f0e59aaefa55184e828370feeda01dc3012c
victor <victorcs2704@gmail.com>
2020-06-12 15:39:16 -0400

    106. Metodo Get - AJAX

--- js/05-ajax.js
+++ js/05-ajax.js
@@ -1,4 +1,12 @@
 $(function () {
 //    Load
-$("#data").load("https://reqres.in/")
+// $("#data").load("https://reqres.in/")
+
+// GET and POST
+    $.get("https://reqres.in/api/users", {page: 2}, function (response) {
+        console.log(response)
+        response.data.forEach((element, index) => {
+            $("#data").append("<p>" + element.first_name + " " + element.last_name + "</p>")
+        })
+    })
 })
